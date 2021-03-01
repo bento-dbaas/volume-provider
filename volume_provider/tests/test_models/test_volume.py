d915965d7044e290d0cd22108fbeb92623fdbe77
William Marquardt <william.marquardt@g.globo>
2021-03-01 16:54:48 -0300

    test create obj in model

--- volume_provider/tests/test_models/test_volume.py
+++ volume_provider/tests/test_models/test_volume.py
@@ -6,13 +6,15 @@ from mongoengine import errors
 from mongoengine import connect
 from volume_provider.settings import *
 
+import bson
+
 connect(MONGODB_DB, **MONGODB_PARAMS)
 
 class PropertiesTestCase(TestCase):
 
     def setUp(self):
         self.volume = Volume()
-        self.volume._data = {
+        self.volume_dct = {
             'id': '123456789abcdef000000000',
             'group': 'fake_name',
             'size_kb': 123,
@@ -21,6 +23,8 @@ class PropertiesTestCase(TestCase):
             'path': '/path/to/volume',
             'owner_address': '/addr'
         }
+
+        self.volume._data = self.volume_dct
 
     def test_return_normal_fields(self):
         self.assertIn('id', self.volume)
@@ -34,4 +38,10 @@ class PropertiesTestCase(TestCase):
         self.assertRaises(errors.ValidationError, self.volume.save)
     
     def test_save_valid_fields(self):
-        self.assertEqual(str(type(self.volume.save().id)), "<class 'bson.objectid.ObjectId'>")
+        self.assertEqual(type(self.volume.save().id), bson.objectid.ObjectId)
+
+    def test_creating_obj(self):
+        self.volume_dct.pop('id')
+        vol = Volume(**self.volume_dct)
+        vol.save()
+        self.assertEqual(type(vol.pk), bson.objectid.ObjectId)
