c6111a040a8b9410fd3eeb2c887fc06cb9384917
Ricardo Dias <ricardo@ricardodias.org>
2021-08-07 20:20:46 -0300

    change rsync command

--- volume_provider/providers/faas.py
+++ volume_provider/providers/faas.py
@@ -120,8 +120,8 @@ die_if_error "Error scp from {snap_dir} to {target_ip}:{target_dir}"
 
     def rsync_script(self, **kw):
         return """
-rsync -e "ssh -i /root/.ssh/dbaas.key"  -aog {source_dir}/.snapshot/{snap_dir}/* root@{target_ip}:{target_dir}
-die_if_error "Error scp from {snap_dir} to {target_ip}:{target_dir}"
+rsync -e "ssh -i /root/.ssh/dbaas.key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"  -aog {source_dir}/.snapshot/{snap_dir}/* root@{target_ip}:{target_dir}
+die_if_error "Error rsync from {snap_dir} to {target_ip}:{target_dir}"
 """.format(**kw)
 
     def _mount(self, volume, fstab=True, *args, **kwargs):
